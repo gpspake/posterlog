87333e38ec4f7596d6d013718fb1e8d2de2d8a3f
gpspake <gpspake@gmail.com>
2015-09-27 09:55:45 -0500

    scaffold admin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,21 @@ get('/', function () {
 
 get('poster', 'PosterController@index');
 get('poster/{slug}', 'PosterController@showPoster');
+
+// Admin area
+get('admin', function () {
+    return redirect('/admin/poster');
+});
+$router->group([
+    'namespace' => 'Admin',
+    'middleware' => 'auth',
+], function () {
+    resource('admin/poster', 'PosterController');
+    resource('admin/tag', 'TagController');
+    get('admin/upload', 'UploadController@index');
+});
+
+// Logging in and out
+get('/auth/login', 'Auth\AuthController@getLogin');
+post('/auth/login', 'Auth\AuthController@postLogin');
+get('/auth/logout', 'Auth\AuthController@getLogout');
